1c3646eb0830fc824dd67909b2a487c50e6858f3
ANSHUMAN H PATEL <96039090+ahp2808@users.noreply.github.com>
2022-05-19 09:37:42 +0000

    problempython

--- ActivitySet01/problem03.py
+++ ActivitySet01/problem03.py
@@ -1,3 +1,6 @@
 # Variables, Expressions & Statements
-
-hrs = float(input("Enter hours? "))
+#hrs = float(input("Enter hours? "))
+hours = input('enter your number of hours: ')
+rate_of_hours = input('enter the rate: ')
+gross_payment = float(hours) * float(rate_of_hours)
+print(f"Rs.{gross_payment} is gross pay for {hours} hours and {rate_of_hours} rate ")
