a9dd8927371a808a84489069f50f7207f073f200
ANSHUMAN H PATEL <96039090+ahp2808@users.noreply.github.com>
2022-05-23 07:04:18 +0000

    pythonproblemsolving

--- ActivitySet01/problem06.py
+++ ActivitySet01/problem06.py
@@ -1,14 +1,31 @@
 # Loops & Iterators
+#program for calc. count and avg of input values
 count = 0
 sum = 0
-while True:
-    num = input("Enter a number: ")
-    # if ( type(num)==int or num == "done" ) == False :
-    #     print('invalid entry')
-    if num == "done":
-        break
-    sum += int(num)
-    count += 1
+try:
+    while True:
+        num = input("Enter a number: ")
+        if num == "done":
+            break
+        sum += int(num)
+        count += 1
+    print(count,sum,sum/count)
+except ValueError :     
+    print('invalid entry')
 
-print(count,sum,sum/count)
+#program for largest and smallest values
+numbers = []
+try:
+    while True:
+        num = input('Enter the number:')
+        numbers.append(int(num))
+        if num == "done":
+            break
+except ValueError:
+    print("Process ended")
+print(numbers)
+numbers.sort()
+print(numbers)
+print(f'largest number is {numbers[-1]}')
+print(f'smallest number is {numbers[0]}')
 
