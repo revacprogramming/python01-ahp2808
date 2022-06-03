ddf01772a6d3c0b230023e073f7a9dcf166a0f89
ANSHUMAN H PATEL <96039090+ahp2808@users.noreply.github.com>
2022-06-03 17:11:45 +0000

    SolvingActSet2

--- ActivitySet02/problem01.py
+++ ActivitySet02/problem01.py
@@ -1,12 +1,8 @@
-
-
-def add(a, b):
-    return  # ...
-
-
-def main():
-    a = input  # ...
-    b = input  # ...
-
-    c = add(a, b)
-    print  # ...
+def add(a,b) :
+    return a+b
+def main() :
+    a = int(input("Enter first number: "))
+    b = int(input("Enter second number: "))
+    c = add(a,b)
+    print(f" Sum of {a} and {b} is {c} ")
+main()
