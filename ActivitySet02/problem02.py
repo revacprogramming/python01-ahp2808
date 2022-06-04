92958afa46e2ce09bf9fa055eb75e3f8bee43423
ANSHUMAN H PATEL <96039090+ahp2808@users.noreply.github.com>
2022-06-04 12:14:11 +0000

    SolvingActSet2

--- ActivitySet02/problem02.py
+++ ActivitySet02/problem02.py
@@ -1,18 +1,15 @@
-
-def add(a, b):
-    pass  # ...
-
-
-def output(a, b, sum):
-    pass  # ...
-
-
+# def main():
+#     a, b = input_two_numbers()
+#     sum = add(a, b)
+#     output(a, b, sum)
+# if __name__ == '__main__': main()
+def add(a,b) :
+    return a+b
+def output(a,b,sum) :
+    print(f"{a} + {b} = {sum}")
 def main():
-    a, b = input_two_numbers()
-    sum = add(a, b)
-
-    output(a, b, sum)
-
-
-if __name__ == '__main__':
+    a, b = input("enter the numbers").split()
+    sum = add(int(a),int(b))
+    output(a,b,sum)
+if __name__ == '__main__' :
     main()
