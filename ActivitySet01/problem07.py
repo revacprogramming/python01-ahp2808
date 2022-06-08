37d2baafbcb70b92351b8f8367eef50ff581feb4
ANSHUMAN H PATEL <96039090+ahp2808@users.noreply.github.com>
2022-06-08 16:38:54 +0000

    py_in_func_format

--- ActivitySet01/problem07.py
+++ ActivitySet01/problem07.py
@@ -1,6 +1,20 @@
-# Strings
-# text= "X-DSPAM-Confidence: 0.8475"
-text = "X-DSPAM-Confidence: 0.8475"
-index = text.find(':')
-number = float(text[index+1:])
-print(number)
+def enter_text() :
+    text = input("Enter the text: ")
+    return text
+
+# text = "X-DSPAM-Confidence: 0.8475 "
+def find_numb(text) :
+
+    index = text.find(':')
+    number = float(text[index+1:])
+    print(number)
+
+def main() :
+    te_xt = enter_text()
+    find_numb(te_xt)
+
+if __name__ =="__main__":
+    try:
+        main()
+    except ValueError :
+        print("Invalid string.Enter a string having numeric value after a ':' ")
