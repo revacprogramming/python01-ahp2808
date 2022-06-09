25c9b7c9ccf163bbf02efdee93b1734431c7d24f
ANSHUMAN H PATEL <96039090+ahp2808@users.noreply.github.com>
2022-06-09 05:42:48 +0000

    py_in_func_format

--- ActivitySet01/problem08.py
+++ ActivitySet01/problem08.py
@@ -1,10 +1,26 @@
-# Files // filename = "dataset/mbox-short.txt"
-file_name = input("Enter name of file: ")
-if file_name == 'progrm8.txt':
-    file_exe = open('progrm8.txt')
+def in_put():
+    file_name = input("Enter name of file: ")
+    return file_name
+
+def open_file(file_name) :
+
+    if file_name == 'progrm8.txt':
+        file_exe = open('progrm8.txt')
+    else:
+        print("Invalid file name")
+    return file_exe
+
+def convert_upper(file_exe) :
+
     for lines in file_exe:
         capitalized = lines.upper()
         print(capitalized)
-else:
-    print("Invalid file name")
-file_exe.close()
+    
+def main() :
+
+    file_name = in_put()
+    file_exe = open_file(file_name)
+    convert_upper(file_exe)
+
+if __name__=='__main__' :
+    main()
