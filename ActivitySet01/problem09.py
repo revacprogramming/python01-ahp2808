2105b2e02fd293bd308646cddb5c03f16ac8d37c
ANSHUMAN H PATEL <96039090+ahp2808@users.noreply.github.com>
2022-06-11 11:33:40 +0000

    py_in_func_format

--- ActivitySet01/problem09.py
+++ ActivitySet01/problem09.py
@@ -1,28 +1,27 @@
-# Lists
-#filename = "dataset/romeo.txt"
-file_name = input("Enter name of file: ")
-uniques=[ ]
-if file_name == 'romeo.txt':
-    file_exe = open('progrm9.txt')
+def in_put():
+    file_name = input("Enter name of file: ")
+    return file_name
+
+def open_file(file_name):
+    if file_name == 'romeo.txt':
+        file_exe = open('progrm9.txt')
+    return file_exe
+def find_unique(file_exe) :    
+    uniques=[]
     for line in file_exe:
         uniq = line.split()
         for word in uniq:
-            if not(word in uniques):
+            if word not in uniques:
                 uniques.append(word)
-uniques.sort()
-print(len(uniques))
-print(uniques)
+    return uniques
 
-#program for collecting email ids
+def main():
+    file_name = in_put()
+    file_exe=open_file(file_name)
+    unique = find_unique(file_exe)
+    unique.sort()
+    print(len(unique))
+    print(unique)
 
-MBOX=[]
-count = 0
-file_name = input("Enter name of file: ")
-if file_name == 'p':
-    file_exe = open('progrm8.txt')
-    for lines in file_exe:
-        if lines.startswith("From ")  :
-            senders = lines.split(" ")
-            print(senders[1])
-            count += 1
-print(f"there were {count} lines having From as a starting word.")
+if __name__=="__main__" :
+    main()
