88a3e83c7cd484363c22c4ea6506c3cb45abfb60
ANSHUMAN H PATEL <96039090+ahp2808@users.noreply.github.com>
2022-07-10 10:19:19 +0000

    pyinfuncformat

--- ActivitySet02/problem06.py
+++ ActivitySet02/problem06.py
@@ -1,6 +1,13 @@
+''' Output:
+    idly 10
+    vada 20'''
+
 class Menu:
-    """fill in class definition here"""
+    def add(self,name,amount):
+        return name,amount
+    def show(self):
+        print(Menu.add)
 m = Menu()  # Menu is a class
-m.add("idly", 10)
-m.add("vada". 20)
+m.add("idli", 10)
+m.add("vada", 20)
 m.show()
