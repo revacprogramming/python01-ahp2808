547f4aea0fc2315a8d00aa78cbdb3ef79727bdbd
ANSHUMAN H PATEL <96039090+ahp2808@users.noreply.github.com>
2022-07-13 13:30:57 +0000

    regex

--- ActivitySet01/problem12.py
+++ ActivitySet01/problem12.py
@@ -1,2 +1,12 @@
-# Regular Expressions
-# https://www.py4e.com/lessons/regex
+import re
+# archives = open('test.txt')
+# for item in archives:
+#     item = item.rstrip()
+#     if re.search('wecome',item):
+#         print(item)
+#stephen.marquard@uct.ac.za
+reqd_file = open('progrm8.txt')
+for item in reqd_file:
+    reqd_ids = re.findall('[a-z,.]+[@]+[a-z,.]+[a-z,.]',item)
+    if reqd_ids != []:
+        print(reqd_ids)
