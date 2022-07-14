bf4e72e6b2dcb82110193a3344c5688fd674ae16
ANSHUMAN H PATEL <96039090+ahp2808@users.noreply.github.com>
2022-07-14 15:18:02 +0000

    Regex2

--- ActivitySet01/problem12.py
+++ ActivitySet01/problem12.py
@@ -1,9 +1,4 @@
 import re
-# archives = open('test.txt')
-# for item in archives:
-#     item = item.rstrip()
-#     if re.search('wecome',item):
-#         print(item)
 #stephen.marquard@uct.ac.za
 reqd_file = open('progrm8.txt')
 for item in reqd_file:
