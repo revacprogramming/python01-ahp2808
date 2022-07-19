83a1b42444a3d96b9db66fcf5a7988dbe150332d
ANSHUMAN H PATEL <96039090+ahp2808@users.noreply.github.com>
2022-07-19 12:06:41 +0000

    area_of_rectangle

---
+++ ActivitySet03/problem01.py
@@ -0,0 +1,42 @@
+import re
+from math import sqrt
+def inp_no_of_rectangle():
+    x = input('Enter the number of rectangles you want area of: ')
+    return int(x)
+
+def inp_coordinates(x):
+    crd=[]
+    str = input(f"Enter three coordinates for the rectangle: ")
+    crd1=re.findall('\d',str)
+    for ele in crd1:
+        crd.append(float(ele))
+    if len(crd1)<6 | len(crd1)>6 :
+        print("Enter six coordinates only")
+    else:
+        return crd
+
+def calc_area(crd):  #c1 = 0,1 c2= 2,3 c3 = 4,5
+    l1 = sqrt(pow(crd[2]-crd[4],2) + pow(crd[3]-crd[5],2))
+    l2 = sqrt(pow(crd[0]-crd[4],2) + pow(crd[1]-crd[5],2))
+    l3 = sqrt(pow(crd[0]-crd[2],2) + pow(crd[1]-crd[3],2))
+    if (l1<l3) & (l2<l3):
+        return l1*l2
+    elif (l2<l1) & (l3<l1):
+        return l2*l3
+    else:
+        return l1*l3
+
+def main():
+    x=inp_no_of_rectangle()
+    n=0
+    while n<x:
+        coords = inp_coordinates(x)
+        final_area = calc_area(coords)
+        print(f'Area of rectangle with vertices{(coords[0],coords[1])},{(coords[2],coords[3])},{(coords[4],coords[5])} is {final_area}')
+        n+=1
+if __name__ == '__main__':
+    main()
+''' Sample input :  1
+    0.0 0.0 0.0 1.0 1.0 0.0
+    Resultant output :
+    Area of rectangle with vertices (0.0,0.0),(0.0,1.0),(1.0,0.0) is 1.0'''
