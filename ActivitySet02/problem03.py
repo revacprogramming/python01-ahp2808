78d519baa5fb484dd8d82926cdbd7b8fa12139a1
ANSHUMAN H PATEL <96039090+ahp2808@users.noreply.github.com>
2022-07-21 03:32:44 +0000

    OOPS

--- ActivitySet02/problem03.py
+++ ActivitySet02/problem03.py
@@ -1,17 +1,22 @@
-reqd_data = ['system','database','username','password']
+#s=system;d=database;u=username;p=password
 def get_cs() :
     cs= input("enter the values: ")
     return cs 
+
 def cs_to_lot(cs) :
-    tupl_list = []
-    n=0
-    while n<len(reqd_data) :
-        tupl_list.append((reqd_data[n],cs[n]))
-        n+=1
-    return tupl_list
+    tup_list = []
+    for value in cs:
+        value = cs.split(';')
+    for ele in value:
+        word = ele.split('=')
+        tup =(word[1],word[0])
+        tup_list.append(tup)
+    return tup_list
+
 def main() :
     cs = get_cs()
     lot = cs_to_lot(cs)
     print(lot)
+
 if __name__ == '__main__':
-    main() 
+    main()
