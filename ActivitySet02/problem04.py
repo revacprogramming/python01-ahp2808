1e130351db8ff182a16e73c86f045cd9386ae4b8
ANSHUMAN H PATEL <96039090+ahp2808@users.noreply.github.com>
2022-07-23 14:43:15 +0000

    OOPS concept

--- ActivitySet02/problem04.py
+++ ActivitySet02/problem04.py
@@ -1,26 +1,25 @@
-reqd_data = ['system','database','username','passwd']
+#s=system;d=database;u=username;p=password
 def get_cs() :
     cs = input("Enter the connected string: ")
     return cs
 def cs_to_lot(cs) :
     tupl_list = []
-    n=0
-    while n<len(reqd_data) :
-        tupl_list.append((reqd_data[n],cs[n]))
-        n +=1
+    for value in cs:
+        value = cs.split(';')
+    for ele in value:
+        word = ele.split('=')
+        tupl_list.append((word[0],word[1]))
     return tupl_list
 def lot_to_cs(lot) :
-    n=0
     cs=''
-    while n< 4 :
-        element = lot[n]
-        cs += (f'{element[0]} = {element[1]}; ')
-        n += 1
-    return cs
+    for ele in lot :
+        cs += (f'{ele[1]} = {ele[0]} ; ')
+    return cs.removesuffix('; ')
 def main() :
     cs = get_cs()
     lot = cs_to_lot(cs)
     print(lot)
     cs = lot_to_cs(lot)
     print(cs)
-main()
+if __name__=='__main__':
+    main()
