4c0fca71866dbe7322250f83b1499cd1523633ee
ANSHUMAN H PATEL <96039090+ahp2808@users.noreply.github.com>
2022-07-24 13:39:36 +0000

    OOPs concept

--- ActivitySet02/problem07.py
+++ ActivitySet02/problem07.py
@@ -1,10 +1,14 @@
+# class Menu:
+#     """fill in class definition here"""
+# m = Menu()
+# m = m + ("idly", 10) + ("vada", 20)  # Hint: operator overloading special methods (__add__, __sub__, etc.)
 
-
+'''idly 10
+vada 20'''
 class Menu:
-    """fill in class definition here"""
-
-
+    def __add__(food,num):
+        orders=()
+        return orders
 m = Menu()
-m = m + ("idly", 10) + ("vada", 20)  # Hint: operator overloading special methods (__add__, __sub__, etc.)
-
-print(m)  # should print the menu properly
+m = m + ("idly", 10) + ("vada", 20) 
+print(m)
